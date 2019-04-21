370062330f88425d18481cc567f525ecfdac42de
LhrET <lhr_ET@outlook.com>
2019-04-21 23:30:35 +0800

    样式更新和主界面设计
    样式更新与主界面设计

--- App/LBLapp/app/src/main/java/net/app/lblpack/puch/MainActivity.java
+++ App/LBLapp/app/src/main/java/net/app/lblpack/puch/MainActivity.java
@@ -1,22 +1,70 @@
 package net.app.lblpack.puch;
 
+import android.support.design.widget.BottomNavigationView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.animation.GlideAnimation;
+import com.bumptech.glide.request.target.ViewTarget;
+
 import net.app.lblpack.common.app.Activity;
+import net.app.lblpack.common.app.Fragment;
+import net.app.lblpack.common.wiget.Portrait;
 
 import butterknife.BindView;
 import butterknife.OnClick;
 
 public class MainActivity extends Activity{
+    @BindView(R.id.appbar)
+    View mLayAppBar;
+    @BindView(R.id.im_portrait)
+    Portrait mPortrait;
+    @BindView(R.id.txt_title)
+    TextView mTextView;
+    @BindView(R.id.lay_container)
+    FrameLayout mFrameLayout;
+    @BindView(R.id.navigation)
+    BottomNavigationView mNavigation;
+
+
+
     @Override
     protected void initData() {
         super.initData();
     }
 
     @Override
+    protected void initWidget() {
+        super.initWidget();
+
+        //this = MainActivity
+        Glide.with(this)
+                .load(R.drawable.bg_src_morning)
+                .centerCrop().into(new ViewTarget<View, GlideDrawable>(mLayAppBar) {
+            @Override
+            public void onResourceReady(GlideDrawable resource, GlideAnimation<? super GlideDrawable> glideAnimation) {
+                //this = new ViewTarget
+                this.view.setBackground(resource.getCurrent());
+            }
+        });
+    }
+
+    @Override
     protected int getContentLayoutId() {
         return R.layout.activity_main;
     }
+    @OnClick(R.id.im_search)
+    void onSearchMenuClick(){
+
+    }
+    @OnClick(R.id.btn_action)
+    void onActionClick(){
+
+    }
+
 }
---
+++ App/LBLapp/common/src/main/java/net/app/lblpack/common/wiget/Portrait.java
@@ -0,0 +1,20 @@
+package net.app.lblpack.common.wiget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class Portrait extends CircleImageView {
+    public Portrait(Context context) {
+        super(context);
+    }
+
+    public Portrait(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Portrait(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+    }
+}
